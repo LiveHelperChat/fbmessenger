ca984767221367055ac24a873904c60b2fb663cc
Remigijus Kiminas <remdex@gmail.com>
2019-03-04 09:11:15 -0500

    Add permision check

--- design/fbmessengertheme/tpl/pagelayouts/parts/modules_menu/extension_module_multiinclude.tpl.php
+++ design/fbmessengertheme/tpl/pagelayouts/parts/modules_menu/extension_module_multiinclude.tpl.php
@@ -1,1 +1,3 @@
+<?php if (erLhcoreClassUser::instance()->hasAccessTo('lhfbmessenger','use_admin')) : ?>
 <li class="nav-item"><a class="nav-link" href="<?php echo erLhcoreClassDesign::baseurl('fbmessenger/index')?>"><i class="material-icons">comment</i><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Facebook chat');?></a></li>
+<?php endif; ?>
