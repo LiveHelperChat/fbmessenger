bb62f8bb3fc8b8f7cab38c970d1f15967f0abd9f
Federico Piazzon <10348142+elplaza@users.noreply.github.com>
2021-10-04 10:12:08 +0200

    Update settings.ini.default.php
    Replace values with environment variables

--- settings/settings.ini.default.php
+++ settings/settings.ini.default.php
@@ -3,9 +3,9 @@
 return array(
     'enable_debug' => false, // Log errors etc in cache/default.log file
     'app_settings' => array (
-        'app_id' => '<app_id>',
-        'app_secret' => '<app_secret>',
-        'verify_token' => '<verify_token>',
+        'app_id' => getenv('FBM_APPID') ?: '', # Facebook Messenger App ID
+        'app_secret' => getenv('FBM_APPSECRET') ?: '', # Facebook Messenger App Secret
+        'verify_token' => getenv('FBM_VERIFYTOKEN') ?: '', # Facebook Messenger Verify Token
     ),
     'elastic_search' => array(
         'search_attr' => 'attr_int_1'
