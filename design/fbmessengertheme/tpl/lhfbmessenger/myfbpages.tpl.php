e1ca1e0f72704f13f324a9308d687b54442f41ee
Remigijus Kiminas <remdex@gmail.com>
2021-10-15 06:10:44 -0400

    UX Changes

--- design/fbmessengertheme/tpl/lhfbmessenger/myfbpages.tpl.php
+++ design/fbmessengertheme/tpl/lhfbmessenger/myfbpages.tpl.php
@@ -1,6 +1,6 @@
 <table class="table" ng-non-bindable>
     <thead>
-        <th colspan="3">Page</th>
+        <th colspan="3"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Page')?></th>
     </thead>
     <?php foreach ($pages['data'] as $page) : ?>
         <tr>
@@ -17,24 +17,24 @@
                         );  echo erLhcoreClassRenderHelper::renderCombobox( $params ); ?></div>
                     <div class="col-6">
                         <?php if (isset($current_pages[$page['id']]) && $current_pages[$page['id']]->enabled == 1) : ?>
-                            <a class="btn btn-sm btn-danger btn-block" href="<?php echo erLhcoreClassDesign::baseurl('fbmessenger/pagesubscribe')?>/<?php echo $page['id']?>/(action)/unsubscribe">Un Subscribe</a>
+                            <a class="btn btn-sm btn-danger btn-block" href="<?php echo erLhcoreClassDesign::baseurl('fbmessenger/pagesubscribe')?>/<?php echo $page['id']?>/(action)/unsubscribe"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Un Subscribe')?></a>
                         <?php else : ?>
-                            <a class="btn btn-sm btn-success btn-block" onclick="document.location = '<?php echo erLhcoreClassDesign::baseurl('fbmessenger/pagesubscribe')?>/<?php echo $page['id']?>/(dep)/'+$('#id_DepartmentID<?php echo $page['id']?>').val()" href="">Subscribe</a>
+                            <a class="btn btn-sm btn-success btn-block" onclick="document.location = '<?php echo erLhcoreClassDesign::baseurl('fbmessenger/pagesubscribe')?>/<?php echo $page['id']?>/(dep)/'+$('#id_DepartmentID<?php echo $page['id']?>').val()" href=""><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Subscribe')?></a>
                         <?php endif; ?>
                     </div>
                 </div>
             </td>
             <td width="1%" style="white-space: nowrap">
                 <?php if (isset($current_pages[$page['id']])) : ?>
-                    <input type="checkbox" onchange="$.get(WWW_DIR_JAVASCRIPT  +'fbmessenger/disablebot/<?php echo $current_pages[$page['id']]->id?>/' + $(this).is(':checked'), function(data) {});" name="bot_disabled" <?php $current_pages[$page['id']]->bot_disabled == 1 ? print 'checked="checked"' : '' ?> > Bot disabled
+                    <input type="checkbox" onchange="$.get(WWW_DIR_JAVASCRIPT  +'fbmessenger/disablebot/<?php echo $current_pages[$page['id']]->id?>/' + $(this).is(':checked'), function(data) {});" name="bot_disabled" <?php $current_pages[$page['id']]->bot_disabled == 1 ? print 'checked="checked"' : '' ?> >&nbsp;<?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Bot disabled')?>
                 <?php endif; ?>
             </td>
             <td width="99%" nowrap="nowrap">
                 <?php echo htmlspecialchars($page['name'])?>
 
                 <?php if (isset($current_pages[$page['id']]) && $current_pages[$page['id']]->enabled == 1) : ?>
-                    <a class="btn btn-secondary btn-xs" href="<?php echo erLhcoreClassDesign::baseurl('fbmessenger/threadmy')?>/<?php echo $current_pages[$page['id']]->id?>/(action)/addbutton" ><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Add start button')?></a>
-                    <a class="btn btn-secondary btn-xs" href="<?php echo erLhcoreClassDesign::baseurl('fbmessenger/threadmy')?>/<?php echo $current_pages[$page['id']]->id?>/(action)/rembutton" ><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Remove start button')?></a>
+                    <a class="btn btn-secondary ml-1 btn-xs" href="<?php echo erLhcoreClassDesign::baseurl('fbmessenger/threadmy')?>/<?php echo $current_pages[$page['id']]->id?>/(action)/addbutton" ><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Add start button')?></a>
+                    <a class="btn btn-secondary ml-1 btn-xs" href="<?php echo erLhcoreClassDesign::baseurl('fbmessenger/threadmy')?>/<?php echo $current_pages[$page['id']]->id?>/(action)/rembutton" ><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Remove start button')?></a>
                 <?php endif; ?>
             </td>
         </tr>
