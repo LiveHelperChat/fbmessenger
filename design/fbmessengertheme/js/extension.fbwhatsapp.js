da82a768e4e5b8e5aa1e8eeaf69156c101bddecc
Remigijus Kiminas <remdex@gmail.com>
2022-12-20 23:59:34 -0500

    fix

--- design/fbmessengertheme/js/extension.fbwhatsapp.js
+++ design/fbmessengertheme/js/extension.fbwhatsapp.js
@@ -1,12 +1,12 @@
 (function() {
     $('#template-to-send').change(function() {
-        $.postJSON(WWW_DIR_JAVASCRIPT + '/fbwhatsapp/rendersend/' + $(this).val() + (businessAccountId ? '/' + businessAccountId : ''), {'data': JSON.stringify(messageFieldsValues)}, function(data) {
+        $.postJSON(WWW_DIR_JAVASCRIPT + '/fbwhatsapp/rendersend/' + $(this).val() + (typeof businessAccountId !== 'undefined' ? '/' + businessAccountId : ''), {'data': JSON.stringify(messageFieldsValues)}, function(data) {
             $('#arguments-template').html(data.preview);
             $('#arguments-template-form').html(data.form);
         });
     });
     if ($('#template-to-send').val() != '') {
-        $.postJSON(WWW_DIR_JAVASCRIPT + '/fbwhatsapp/rendersend/' + $('#template-to-send').val() + (businessAccountId ? '/' + businessAccountId : ''), {'data': JSON.stringify(messageFieldsValues)}, function(data) {
+        $.postJSON(WWW_DIR_JAVASCRIPT + '/fbwhatsapp/rendersend/' + $('#template-to-send').val() + (typeof businessAccountId !== 'undefined' ? '/' + businessAccountId : ''), {'data': JSON.stringify(messageFieldsValues)}, function(data) {
             $('#arguments-template').html(data.preview);
             $('#arguments-template-form').html(data.form);
         });
