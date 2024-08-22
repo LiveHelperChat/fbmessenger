7d4eff3c9f249c99ca7664fe03cbb22f19532bc7
Remigijus Kiminas <remdex@gmail.com>
2024-08-22 08:54:22 -0400

    Progress

--- modules/lhfbmessenger/fbcallback.php
+++ modules/lhfbmessenger/fbcallback.php
@@ -7,14 +7,14 @@ $fb = erLhcoreClassModelFBMessengerUser::getFBAppInstance();
 $tpl->set('fb',$fb);
 
 try {
-	$Result['content'] = $tpl->fetch();
+    $Result['content'] = $tpl->fetch();
 } catch (Exception $e) {
-	echo $e->getMessage();
-	exit;
+    echo $e->getMessage();
+    exit;
 }
 
 $Result['path'] = array(
-		array('url' =>erLhcoreClassDesign::baseurl('fbmessenger/list'), 'title' => erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Facebook pages'))
+    array('url' =>erLhcoreClassDesign::baseurl('fbmessenger/list'), 'title' => erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Facebook pages'))
 );
 
 ?>
