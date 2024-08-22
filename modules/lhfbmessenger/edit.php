2043ddbc10a4b8055774ab5796f89538b69e7fb1
Remigijus Kiminas <remdex@gmail.com>
2024-08-22 08:52:23 -0400

    Progress

--- modules/lhfbmessenger/edit.php
+++ modules/lhfbmessenger/edit.php
@@ -10,7 +10,12 @@ if (ezcInputForm::hasPostData()) {
         erLhcoreClassModule::redirect('fbmessenger/list');
         exit ;
     }
-    
+
+    if (!isset($_POST['csfr_token']) || !$currentUser->validateCSFRToken($_POST['csfr_token'])) {
+        erLhcoreClassModule::redirect('fbmessenger/list');
+        exit;
+    }
+
     $Errors = erLhcoreClassFBValidator::validatePage($item);
 
     if (count($Errors) == 0) {
@@ -37,7 +42,7 @@ $Result['content'] = $tpl->fetch();
 
 $Result['path'] = array(
     array (
-        'url' =>erLhcoreClassDesign::baseurl('fbmessenger/list'), 
+        'url' => erLhcoreClassDesign::baseurl('fbmessenger/list'),
         'title' => erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Facebook pages')        
     ),
     array (       
