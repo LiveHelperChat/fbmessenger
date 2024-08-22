6582762ea2ac63e86b3b4bea297608e6e054f5c0
Remigijus Kiminas <remdex@gmail.com>
2024-08-22 08:21:47 -0400

    Progress

--- design/fbmessengertheme/tpl/lhfbmessenger/instagramchannels.tpl.php
+++ design/fbmessengertheme/tpl/lhfbmessenger/instagramchannels.tpl.php
@@ -7,24 +7,21 @@
 <h1><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Facebook messenger');?></h1>
 
 <ul>
-    <?php /*<li><a href="<?php echo erLhcoreClassDesign::baseurl('fbmessenger/bot')?>"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Bot setup');?></a></li>*/ ?>
-
     <?php
 
         $fb = erLhcoreClassModelFBMessengerUser::getFBAppInstance();
 
         $helper = $fb->getRedirectLoginHelper();
-
-        $permissions = ['instagram_manage_messages', 'instagram_basic', 'pages_manage_metadata', 'pages_read_engagement']; // Optional permissions
+    
+        $permissions = ['email', 'pages_show_list', 'pages_messaging', 'instagram_manage_messages', 'instagram_basic', 'pages_manage_metadata']; // Optional permissions
 
         $time = time();
         $hash = sha1(erConfigClassLhConfig::getInstance()->getSetting('site','seller_secret_hash') . '_' . erConfigClassLhConfig::getInstance()->getSetting('site','seller_subdomain') . '_' .  erLhcoreClassInstance::getInstance()->id . '_' . erLhcoreClassUser::instance()->getUserID() . $time);
         $loginUrl = 'https://'.  erConfigClassLhConfig::getInstance()->getSetting('site','seller_subdomain') . '.' . erConfigClassLhConfig::getInstance()->getSetting('site','seller_domain') . erLhcoreClassDesign::baseurl('fbmessenger/fblogininstanceinstagram') . '/' . erLhcoreClassInstance::getInstance()->id . '/' . erLhcoreClassUser::instance()->getUserID() . '/' . $time . '/' . $hash;
 
-        echo '<li><a href="' . htmlspecialchars($loginUrl) . '"><img height="30" src="/extension/fbmessenger/design/fbmessengertheme/images/fblogin.png" /></a></li>';
+        echo '<li><a href="' . htmlspecialchars($loginUrl) . '"><img height="30" src="'. erLhcoreClassDesign::design('images/fblogin.png') .'" /></a></li>';
     ?>
 
-    <li><a href="<?php echo erLhcoreClassDesign::baseurl('chatbot/chatbot')?>?page=chatbot-builder"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Chatbot');?></a></li>
     <li><a href="<?php echo erLhcoreClassDesign::baseurl('fbmessenger/bbcode')?>"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','BBCode');?></a></li>
 </ul>
 
--- design/fbmessengertheme/tpl/lhfbmessenger/leads.tpl.php
+++ design/fbmessengertheme/tpl/lhfbmessenger/leads.tpl.php
@@ -1,56 +1,68 @@
-<div class="row">
-    <div class="col-12">
-        <?php $fbmessenger_module_enabled_pre = !class_exists('erLhcoreClassInstance') || erLhcoreClassInstance::getInstance()->feature_2_supported == 1;?>
-        
-        <?php if ($fbmessenger_module_enabled_pre === false) : $errors[] = 'Module not supported'; ?>
-            <?php include(erLhcoreClassDesign::designtpl('lhkernel/validation_error.tpl.php'));?>
-            <?php return; endif; ?>
+<?php $fbmessenger_module_enabled_pre = !class_exists('erLhcoreClassInstance') || erLhcoreClassInstance::getInstance()->feature_2_supported == 1;?>
 
-        <h1><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Facebook leads list');?></h1>
+<?php if ($fbmessenger_module_enabled_pre === false) : $errors[] = 'Module not supported'; ?>
+    <?php include(erLhcoreClassDesign::designtpl('lhkernel/validation_error.tpl.php'));?>
+    <?php return; endif; ?>
 
-        <?php if (isset($items)) : ?>
+<h1><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Facebook leads list');?></h1>
 
-            <table cellpadding="0" cellspacing="0" class="table" width="100%">
-                <thead>
-                <tr>
-                    <th><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Photo');?></th>
-                    <th><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Facebook User ID');?></th>
-                    <th><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Gender');?></th>
-                    <th><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Name');?></th>
-                    <th><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Surname');?></th>
-                    <th><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Subscribe');?></th>
-                    <th><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Status');?></th>
-                    <th><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Locale');?></th>
-                    <th width="1%"></th>
-                </tr>
-                </thead>
-                <?php foreach ($items as $item) : ?>
-                    <tr>
-                        <td>
-                            <?php if ($item->profile_pic == '') : ?>
-                            N/A
-                            <?php else : ?>
-                            <img class="img-responsive" width="60" src="<?php echo $item->profile_pic_front?>" />
-                            <?php endif; ?>
-                        </td>
-                        <td><?php echo $item->user_id?></td>
-                        <td><?php echo ucfirst($item->gender)?></td>
-                        <td><?php echo htmlspecialchars($item->first_name)?></td>
-                        <td><?php echo htmlspecialchars($item->last_name)?></td>
-                        <td><?php echo htmlspecialchars($item->subscribe_channels)?></td>
-                        <td><?php echo htmlspecialchars($item->auto_stop)?></td>
-                        <td><?php echo $item->locale?></td>
-                        <td><a class="btn btn-danger btn-xs csfr-required" onclick="return confirm('Are you sure you want to delete this record?')" href="<?php echo erLhcoreClassDesign::baseurl('fbmessenger/deletelead')?>/<?php echo $item->id?>">Delete</a></td>
-                    </tr>
-                <?php endforeach; ?>
-            </table>
+<?php if (isset($items)) : ?>
 
-            <?php include(erLhcoreClassDesign::designtpl('lhkernel/secure_links.tpl.php')); ?>
+    <table cellpadding="0" cellspacing="0" class="table table-sm" width="100%" ng-non-bindable>
+        <thead>
+        <tr>
+            <th><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','ID');?></th>
+            <th><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Photo');?></th>
+            <th><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Page');?></th>
+            <th><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','User ID');?></th>
+            <th><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','E-mail');?></th>
+            <th><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Phone');?></th>
+            <th><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Gender');?></th>
+            <th><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Name');?></th>
+            <th><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Surname');?></th>
+            <th><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Subscribe');?></th>
+            <th><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Status');?></th>
+            <th><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Locale');?></th>
+            <th><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Department');?></th>
+            <th><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('module/fbmessenger','Status');?></th>
+        </tr>
+        </thead>
+        <?php foreach ($items as $item) : ?>
+            <tr>
+                <td><?php echo htmlspecialchars($item->id)?></td>
+                <td>
+                    <?php if ($item->profile_pic != '') : ?>
+                    <img class="img-responsive" width="60" src="<?php echo $item->profile_pic_front?>" />
+                    <?php else : ?>
+                    N/A
+                    <?php endif; ?>
+                </td>
+                <td><?php echo htmlspecialchars($item->page_id)?></td>
+                <td><?php echo htmlspecialchars($item->user_id)?></td>
+                <td><?php echo htmlspecialchars($item->email)?></td>
+                <td><?php echo htmlspecialchars($item->phone)?></td>
+                <td><?php echo ucfirst($item->gender)?></td>
+                <td><?php echo htmlspecialchars($item->first_name)?></td>
+                <td><?php echo htmlspecialchars($item->last_name)?></td>
+                <td><?php echo htmlspecialchars($item->subscribe_channels)?></td>
+                <td><?php echo htmlspecialchars($item->auto_stop)?></td>
+                <td><?php echo htmlspecialchars($item->locale)?></td>
+                <td><?php echo htmlspecialchars($item->dep)?></td>
+                <td>
+                    <?php if ($item->blocked == 1) : ?>
+                        <i title="Blocked" class="material-icons chat-closed">&#xE14B;</i>
+                    <?php else : ?>
+                        <i title="Ok" class="material-icons chat-active">&#xE5CA;</i>
+                    <?php endif; ?>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
 
-            <?php if (isset($pages)) : ?>
-                <?php include(erLhcoreClassDesign::designtpl('lhkernel/paginator.tpl.php')); ?>
-            <?php endif;?>
+    <?php include(erLhcoreClassDesign::designtpl('lhkernel/secure_links.tpl.php')); ?>
 
-        <?php endif;?>
-    </div>
-</div>
+    <?php if (isset($pages)) : ?>
+        <?php include(erLhcoreClassDesign::designtpl('lhkernel/paginator.tpl.php')); ?>
+    <?php endif;?>
+
+<?php endif;?>
