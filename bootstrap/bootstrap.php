1e30ec88b8306372e218dc01e4f64e84fb4c8535
Remigijus Kiminas <remdex@gmail.com>
2024-08-23 05:08:24 -0400

    Callback

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -191,7 +191,284 @@ class erLhcoreClassExtensionFbmessenger {
             }
         }
     }
-    
+
+    /*
+     * Store Initial Message within chat
+     * */
+    public function incommingChatStarted($params)
+    {
+        if (
+            isset($params['data']['object']) &&
+            isset($params['data']['entry']) &&
+            $params['data']['object'] == 'whatsapp_business_account'
+        ) {
+            $initMessageFound = false;
+            foreach ($params['data']['entry'] as $entryItem) {
+                foreach ($entryItem['changes'] as $changeItem) {
+                    if (isset($changeItem['value']['messages'])) {
+                        foreach ($changeItem['value']['messages'] as $messageItem) {
+                            $fbWhatsAppMessage = \LiveHelperChatExtension\fbmessenger\providers\erLhcoreClassModelMessageFBWhatsAppMessage::findOne([
+                                'sort' => '`id` DESC',
+                                'filter' => [
+                                    'phone' => $messageItem['from'],
+                                    'phone_sender_id' => $changeItem['value']['metadata']["phone_number_id"]
+                                ]
+                            ]);
+                            // We insert last message only if it does not have chat assigned already
+                            // This is change since multiple WhatsApp accounts support was added.
+                            if (is_object($fbWhatsAppMessage) && $fbWhatsAppMessage->dep_id > 0 && $fbWhatsAppMessage->chat_id == 0) {
+                                // Chat
+                                $params['chat']->dep_id = $fbWhatsAppMessage->dep_id;
+                                $params['chat']->updateThis(['update' => ['dep_id']]);
+
+                                // Save template message first before saving initial response in the lhc core
+                                $msg = new erLhcoreClassModelmsg();
+                                $msg->msg = $fbWhatsAppMessage->message;
+                                $msg->chat_id = $params['chat']->id;
+                                $msg->user_id = $fbWhatsAppMessage->user_id;
+                                $msg->time = $fbWhatsAppMessage->created_at;
+                                erLhcoreClassChat::getSession()->save($msg);
+
+                                // Update message bird
+                                $fbWhatsAppMessage->chat_id = $params['chat']->id;
+                                $fbWhatsAppMessage->updateThis(['update' => ['chat_id']]);
+                                $initMessageFound = true;
+
+                                // Update campaign related records
+                                if ($fbWhatsAppMessage->campaign_recipient_id > 0) {
+                                    $campaignRecipient = \LiveHelperChatExtension\fbmessenger\providers\erLhcoreClassModelMessageFBWhatsAppCampaignRecipient::fetch($fbWhatsAppMessage->campaign_recipient_id);
+                                    if (is_object($campaignRecipient)) {
+                                        $campaignRecipient->conversation_id = $fbWhatsAppMessage->chat_id;
+                                        $campaignRecipient->updateThis(['update' => ['conversation_id']]);
+                                    }
+                                }
+
+                                // Update contact main attributes
+                                if ($fbWhatsAppMessage->recipient_id > 0) {
+                                    $contact = \LiveHelperChatExtension\fbmessenger\providers\erLhcoreClassModelMessageFBWhatsAppContact::fetch($fbWhatsAppMessage->recipient_id);
+                                    if (is_object($contact)) {
+                                        $contact->chat_id = $fbWhatsAppMessage->chat_id;
+                                        $contact->updateThis(['update' => ['chat_id']]);
+                                    }
+                                }
+                            }
+                        }
+
+                        if ($initMessageFound == false) {
+                            $businessAccount = \LiveHelperChatExtension\fbmessenger\providers\erLhcoreClassModelMessageFBWhatsAppAccount::findOne(array('customfilter' => array("JSON_CONTAINS(`phone_number_ids`,'\"" . (int)$changeItem['value']['metadata']["phone_number_id"] . "\"','$')" )));
+                            // Override only if we found separate business account for that phone number
+                            if (is_object($businessAccount)) {
+                                $params['chat']->dep_id = isset($businessAccount->phone_number_deps_array[(string)$changeItem['value']['metadata']["phone_number_id"]]) ? $businessAccount->phone_number_deps_array[(string)$changeItem['value']['metadata']["phone_number_id"]] : $businessAccount->dep_id;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public function incommingWebhook($params)
+    {
+        if (
+            isset($params['data']['object']) &&
+            isset($params['data']['entry']) &&
+            $params['data']['object'] == 'whatsapp_business_account'
+        ) {
+            $skippedMessage = false;
+            foreach ($params['data']['entry'] as $entryItem) {
+                foreach ($entryItem['changes'] as $changeItem) {
+                    if (isset($changeItem['value']['statuses'])) {
+                        foreach ($changeItem['value']['statuses'] as $statusItem) {
+                            $fbWhatsAppMessage = \LiveHelperChatExtension\fbmessenger\providers\erLhcoreClassModelMessageFBWhatsAppMessage::findOne(['filter' => ['fb_msg_id' => $statusItem['id']]]);
+                            if ($fbWhatsAppMessage instanceof \LiveHelperChatExtension\fbmessenger\providers\erLhcoreClassModelMessageFBWhatsAppMessage) {
+
+                                $statusMap = [
+                                    'pending' => \LiveHelperChatExtension\fbmessenger\providers\erLhcoreClassModelMessageFBWhatsAppMessage::STATUS_PENDING,
+                                    'sent' => \LiveHelperChatExtension\fbmessenger\providers\erLhcoreClassModelMessageFBWhatsAppMessage::STATUS_SENT,
+                                    'delivered' => \LiveHelperChatExtension\fbmessenger\providers\erLhcoreClassModelMessageFBWhatsAppMessage::STATUS_DELIVERED,
+                                    'read' => \LiveHelperChatExtension\fbmessenger\providers\erLhcoreClassModelMessageFBWhatsAppMessage::STATUS_READ,
+                                    'rejected' => \LiveHelperChatExtension\fbmessenger\providers\erLhcoreClassModelMessageFBWhatsAppMessage::STATUS_REJECTED,
+                                ];
+
+                                if (isset($statusItem['conversation']['id'])) {
+                                    $fbWhatsAppMessage->conversation_id = $statusItem['conversation']['id'];
+                                }
+
+                                if ($fbWhatsAppMessage->status != \LiveHelperChatExtension\fbmessenger\providers\erLhcoreClassModelMessageFBWhatsAppMessage::STATUS_READ) {
+                                    $fbWhatsAppMessage->status = $statusMap[$statusItem['status']];
+                                }
+
+                                if ($fbWhatsAppMessage->status == \LiveHelperChatExtension\fbmessenger\providers\erLhcoreClassModelMessageFBWhatsAppMessage::STATUS_REJECTED) {
+                                    $fbWhatsAppMessage->send_status_raw = $fbWhatsAppMessage->send_status_raw . json_encode($params['data']);
+                                }
+
+                                if ($fbWhatsAppMessage->campaign_recipient_id > 0) {
+                                    $campaignRecipient = \LiveHelperChatExtension\fbmessenger\providers\erLhcoreClassModelMessageFBWhatsAppCampaignRecipient::fetch($fbWhatsAppMessage->campaign_recipient_id);
+                                    if (is_object($campaignRecipient)) {
+                                        if ($campaignRecipient->status != \LiveHelperChatExtension\fbmessenger\providers\erLhcoreClassModelMessageFBWhatsAppMessage::STATUS_READ) {
+                                            $campaignRecipient->status = $statusMap[$statusItem['status']];
+
+                                            if ($campaignRecipient->status == \LiveHelperChatExtension\fbmessenger\providers\erLhcoreClassModelMessageFBWhatsAppMessage::STATUS_READ) {
+                                                $campaignRecipient->opened_at = time();
+                                            }
+
+                                            $campaignRecipient->updateThis(['update' => ['status', 'opened_at']]);
+                                        }
+                                    }
+                                }
+
+                                $chatId = 0;
+
+                                // Insert message as a normal message to the last chat customer had
+                                // In case there is chosen reopen old chat
+                                // Which by the case is the default option of the extension
+                                if (in_array($fbWhatsAppMessage->status,[
+                                        \LiveHelperChatExtension\fbmessenger\providers\erLhcoreClassModelMessageFBWhatsAppMessage::STATUS_READ
+                                    ]) && $fbWhatsAppMessage->chat_id == 0) {
+                                    $presentConversation = \LiveHelperChatExtension\fbmessenger\providers\erLhcoreClassModelMessageFBWhatsAppMessage::findOne([
+                                        'filter' => [
+                                            'phone_sender_id' => $fbWhatsAppMessage->phone_sender_id,
+                                            'phone' => $fbWhatsAppMessage->phone
+                                        ],
+                                        'filternot' => [
+                                            'chat_id' => 0
+                                        ],
+                                        'sort' => '`id` DESC'
+                                    ]);
+
+                                    if (is_object($presentConversation)) {
+                                        $chat = erLhcoreClassModelChat::fetch($presentConversation->chat_id);
+                                        if (is_object($chat)) {
+                                            // Save template message first before saving initial response in the lhc core
+                                            $msg = new erLhcoreClassModelmsg();
+                                            $msg->msg = $fbWhatsAppMessage->message;
+                                            $chatId = $msg->chat_id = $chat->id;
+                                            $msg->user_id = $fbWhatsAppMessage->user_id;
+                                            $msg->time = $fbWhatsAppMessage->created_at;
+                                            erLhcoreClassChat::getSession()->save($msg);
+
+                                            $chat->last_msg_id = $msg->id;
+                                            $chat->updateThis(['update' => ['last_msg_id']]);
+                                        }
+                                    } else { // Try to find any on-going chat
+                                        $chatIdExternal = $fbWhatsAppMessage->phone;
+
+                                        $conditions = $params['webhook']->conditions_array;
+
+                                        if (isset($conditions['chat_id_preg_rule']) && $conditions['chat_id_preg_rule'] != '') {
+                                            $chatIdExternal = preg_replace($conditions['chat_id_preg_rule'], $conditions['chat_id_preg_value'], $chatIdExternal);
+                                        }
+
+                                        $incomingChat = erLhcoreClassModelChatIncoming::findOne(array('filter' => array('chat_external_id' => $chatIdExternal . '__' . $fbWhatsAppMessage->phone_sender_id)));
+
+                                        if ($incomingChat instanceof erLhcoreClassModelChatIncoming && is_object($incomingChat->chat)) {
+                                            $msg = new erLhcoreClassModelmsg();
+                                            $msg->msg = $fbWhatsAppMessage->message;
+                                            $chatId = $msg->chat_id = $incomingChat->chat->id;
+                                            $msg->user_id = $fbWhatsAppMessage->user_id;
+                                            $msg->time = $fbWhatsAppMessage->created_at;
+                                            erLhcoreClassChat::getSession()->save($msg);
+
+                                            $incomingChat->chat->last_msg_id = $msg->id;
+                                            $incomingChat->chat->updateThis(['update' => ['last_msg_id']]);
+                                        }
+                                    }
+                                }
+
+                                $fbWhatsAppMessage->saveThis();
+
+                                // Update contact main attributes
+                                if ($fbWhatsAppMessage->recipient_id > 0) {
+                                    $contact = \LiveHelperChatExtension\fbmessenger\providers\erLhcoreClassModelMessageFBWhatsAppContact::fetch($fbWhatsAppMessage->recipient_id);
+
+                                    if (is_object($contact)) {
+
+                                        $contact->chat_id = $fbWhatsAppMessage->chat_id > 0 ? $fbWhatsAppMessage->chat_id : $chatId;
+
+                                        if ($fbWhatsAppMessage->status == \LiveHelperChatExtension\fbmessenger\providers\erLhcoreClassModelMessageFBWhatsAppMessage::STATUS_REJECTED) {
+                                            $contact->delivery_status = \LiveHelperChatExtension\fbmessenger\providers\erLhcoreClassModelMessageFBWhatsAppContact::DELIVERY_STATUS_FAILED;
+                                        }
+
+                                        if (in_array((int)$fbWhatsAppMessage->status,[
+                                                \LiveHelperChatExtension\fbmessenger\providers\erLhcoreClassModelMessageFBWhatsAppMessage::STATUS_READ,
+                                                \LiveHelperChatExtension\fbmessenger\providers\erLhcoreClassModelMessageFBWhatsAppMessage::STATUS_DELIVERED
+                                            ]) &&
+                                            in_array((int)$contact->delivery_status,[
+                                                \LiveHelperChatExtension\fbmessenger\providers\erLhcoreClassModelMessageFBWhatsAppContact::DELIVERY_STATUS_FAILED,
+                                                \LiveHelperChatExtension\fbmessenger\providers\erLhcoreClassModelMessageFBWhatsAppContact::DELIVERY_STATUS_UNKNOWN
+                                            ])
+                                        ) {
+                                            $contact->delivery_status = \LiveHelperChatExtension\fbmessenger\providers\erLhcoreClassModelMessageFBWhatsAppContact::DELIVERY_STATUS_ACTIVE;
+                                        }
+
+                                        $contact->updateThis(['update' => ['delivery_status', 'chat_id']]);
+                                    }
+                                }
+
+                                if (isset($campaignRecipient) && is_object($campaignRecipient) && ($fbWhatsAppMessage->chat_id > 0 ||  $chatId > 0)) {
+                                    $campaignRecipient->conversation_id = $fbWhatsAppMessage->chat_id > 0 ? $fbWhatsAppMessage->chat_id : $chatId;
+                                    $campaignRecipient->updateThis(['update' => ['conversation_id']]);
+                                }
+
+                            } else { // It was normal message delivery status change
+
+                                $conditions = $params['webhook']->conditions_array;
+
+                                $chatIdExternal = $statusItem['recipient_id'];
+
+                                if (isset($conditions['chat_id_preg_rule']) && $conditions['chat_id_preg_rule'] != '') {
+                                    $chatIdExternal = preg_replace($conditions['chat_id_preg_rule'], $conditions['chat_id_preg_value'], $chatIdExternal);
+                                }
+
+                                $incomingChat = erLhcoreClassModelChatIncoming::findOne(array('filter' => array('chat_external_id' => $chatIdExternal.'__'.$changeItem['value']['metadata']['phone_number_id'])));
+
+                                // Chat was found, now we need to find exact message
+                                if ($incomingChat instanceof erLhcoreClassModelChatIncoming && is_object($incomingChat->chat)) {
+
+                                    $statusMap = [
+                                        'pending' => erLhcoreClassModelmsg::STATUS_PENDING,
+                                        'sent' => erLhcoreClassModelmsg::STATUS_SENT,
+                                        'delivered' => erLhcoreClassModelmsg::STATUS_DELIVERED,
+                                        'read' =>  erLhcoreClassModelmsg::STATUS_READ,
+                                        'rejected' =>  erLhcoreClassModelmsg::STATUS_REJECTED
+                                    ];
+
+                                    $msg = erLhcoreClassModelmsg::findOne(['filter' => ['chat_id' => $incomingChat->chat->id],'customfilter' => ['`meta_msg` != \'\' AND JSON_EXTRACT(meta_msg,\'$.iwh_msg_id\') = ' . ezcDbInstance::get()->quote($statusItem['id'])]]);
+
+                                    if (is_object($msg) && $msg->del_st != erLhcoreClassModelmsg::STATUS_READ) {
+
+                                        $msg->del_st = max($statusMap[$statusItem['status']],$msg->del_st);
+                                        $msg->updateThis(['update' => ['del_st']]);
+
+                                        // Refresh message delivery status for op
+                                        $chat = $incomingChat->chat;
+                                        $chat->operation_admin .= "lhinst.updateMessageRowAdmin({$msg->chat_id},{$msg->id});";
+                                        if ($msg->del_st == erLhcoreClassModelmsg::STATUS_READ) {
+                                            $chat->has_unread_op_messages = 0;
+                                        }
+                                        $chat->updateThis(['update' => ['operation_admin','has_unread_op_messages']]);
+
+                                        // NodeJS to update message delivery status
+                                        erLhcoreClassChatEventDispatcher::getInstance()->dispatch('chat.message_updated', array('msg' => & $msg, 'chat' => & $chat));
+                                    }
+
+                                }
+                            }
+                        }
+                    } else {
+                        $skippedMessage = true;
+                    }
+                }
+            }
+
+            // There was only our processed messages in the callback
+            // No need to process anything else
+            if ($skippedMessage === false) {
+                exit;
+            }
+        }
+    }
+
     public function updateWhatsAppDepartment($params)
     {
         if (isset($params['data']['entry'][0]['changes'][0]['value']['metadata']['phone_number_id']) && is_numeric($params['data']['entry'][0]['changes'][0]['value']['metadata']['phone_number_id'])) {
